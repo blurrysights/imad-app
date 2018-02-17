c7942ccbc3d03df36609e15eee7e6fa4f15db314
blurrysights <36415952+blurrysights@users.noreply.github.com>
2018-02-17 23:42:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,11 +53,7 @@ submit.onclick = function() {
         //not done yet
     };
     //make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://shreyagorantla5.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
-
+   
    
     
 };
